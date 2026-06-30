1bc9ba3bb719514a6f1508c4abc1f883f17d4b1b
Jiayu Hu <jiayu.hu@inkbridge.io>
2026-06-30 15:20:56 -0400

    Bump opencensus to >= 0.11.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'grpcio>=1.47.0',
         'opencensus-ext-stackdriver==0.8.0',
         'opencensus-ext-prometheus',
-        'opencensus==0.10.0',
+        'opencensus>=0.11.4',
         'python-ldap',
         'pyyaml',
     ],
