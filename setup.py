9e83aca6baec88f765e268cc0b673f84311e3616
Jiayu Hu <jiayu.hu@inkbridge.io>
2026-08-06 13:40:30 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.19'
+VERSION = '0.0.20'
 DESCRIPTION = 'OpenLDAP OpenCensus Statistics'
 LONG_DESCRIPTION = 'A package to gather statistics for OpenLDAP and publish via OpenCensus.'
 
